928f7daa72102823d9b2451239eb70f8376df8f4
Rut Juniati Gagas <rut.juniati.gagas@student.untan.ac.id>
2018-03-21 13:07:37 +0700

    latihan 1. halaman perdana

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//ini adalah komentar saja
+// dilarang komen
+//kalau like boleh
+
+	echo "Rut Juniati Gagas";
+	echo "H1101161007";
+
+	echo "hello world";
+	echo "hello world";
+
+	
+    
+?>
